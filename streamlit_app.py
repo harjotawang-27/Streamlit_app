bd344590e1001968732edf296dd914001b79a44e
harjotawang-27 <selisihberatlazada@gmail.com>
2025-06-16 11:34:17 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,6 +108,9 @@ selected_gerai = st.multiselect(
 # Filter kurir berdasarkan gerai terpilih
 kurir_filtered = filtered_kurir[filtered_kurir['NAMA_GERAI'].isin(selected_gerai)]
 
+st.markdown("<br><br>", unsafe_allow_html=True)
+st.markdown("###ℹ️ Tabel diatas menampilkan jumlah produktifitas kurir dan insentif yang didapat pada periode tertentu, sehingga dapat dilihat Si Gesit yang mempunyai performa baik atau tidak ")
+st.markdown("###ℹ️ Chart SLA Pickup Gerai menunjukan performa pickup setiap gerai pada periode tertentu untuk mengukur performa perbandingan SLA Pickup setiap Gerai  ")
 # Jika kosong
 if kurir_filtered.empty:
     st.warning("Tidak ada data untuk gerai yang dipilih.")
